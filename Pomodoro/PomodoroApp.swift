c409b2babd5329c02737ebc7f884040fb1c647aa
yohan <beginnerdevyohan@gmail.com>
2025-02-02 18:43:19 -0500

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Yohan Yoon on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
